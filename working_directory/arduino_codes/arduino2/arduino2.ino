1c48cca1a2b3529b5b06ee481d76e9fa365fa217
ironstein <ironstein1994@gmail.com>
2016-02-12 10:33:19 +0530

    Completed arduino2.py and arduino2.ino

--- working_directory/arduino_codes/arduino2/arduino2.ino
+++ working_directory/arduino_codes/arduino2/arduino2.ino
@@ -1,78 +1,116 @@
 #include<Servo.h>
-int x,y,z;
 
-float pick_val=45;
-float place_val=140;
-int vert_down_angle = 95;
-int vert_up_angle = 140;
-//change for adjustments
+//-------------- SERVO CALIBRATED POSITIONS ------------------ 
+float gripper_servo_grab_angle=20;
+float gripper_servo_release_angle=100;
+int vertical_servo_down_angle = 75;
+int vertical_servo_up_angle = 140;
 
-int rot_servo_pin=6;
-int vert_servo_pin=5;
-int hand_servo_pin=3;
-//change on basis of connections
+int gripper_servo_initial_position = 90;
+int vertical_servo_initial_position = 120;
+int rotation_servo_initial_position = 90;
+//-------------- SERVO PIN ATTACHMENTS --------------------
+int vertical_servo_pin=9;
+int rotation_servo_pin=10;
+int gripper_servo_pin=3;
 
-int comma=222;
-int com_pick=200;
-int com_place=211;
-//one needs to be odd and the other even
+//-------------- COMMUNICATION BYTES ------------------------
+int start_byte = 255;  // "\xff"
+int pick_command = 97; // "a"
+int place_command= 98; // "b"
+int move_command = 99; // "c"
+char OKAY_CHARACTER = 'O';
+char DONE_CHARACTER = 'D';
+char NOT_OKAY_CHARACTER = 'N';
+int DELAY = 3000;
 
-Servo rot_servo;
-Servo vert_servo;
-Servo hand_servo;
+Servo rotation_servo;
+Servo vertical_servo;
+Servo gripper_servo;
 
-void arm_sequence(float p,int s){
-  float f;
-  if(s==0){
-    f=pick_val;
-  }
-  if(s==1){
-    f=place_val;
-  }
-  rot_servo.write(p);
-  digitalWrite(13,HIGH);
-  delay(1000);
-  vert_servo.write(vert_down_angle);
-  digitalWrite(13,LOW);
-  delay(1000);
-  hand_servo.write(f);
-  digitalWrite(13,HIGH);
-  delay(1000);
-  vert_servo.write(vert_up_angle);
-  digitalWrite(13,LOW);
-  delay(1000);
-}
+int COUNT = 0;
 
 void setup(){
   Serial.begin(57600);
-  rot_servo.attach(rot_servo_pin);
-  vert_servo.attach(vert_servo_pin);
-  hand_servo.attach(hand_servo_pin);
-  vert_servo.write(vert_up_angle);
+  //attach
+  rotation_servo.attach(rotation_servo_pin);
+  vertical_servo.attach(vertical_servo_pin);
+  gripper_servo.attach(gripper_servo_pin);
+
+  //move servos to initial positions
+  rotation_servo.write(rotation_servo_initial_position);
+  vertical_servo.write(vertical_servo_initial_position);
+  gripper_servo.write(gripper_servo_initial_position);
+
   pinMode(13,OUTPUT);
-//  digitalWrite(13,LOW);
-//  delay(1000);
-//  digitalWrite(13,HIGH);
-//  delay(1000);
-//  digitalWrite(13,LOW);
-//  delay(1000);
+  digitalWrite(13,LOW);
 }
 
 void loop(){
-  if(Serial.available()==3){
-    x= Serial.read();
-    y= Serial.read();
-    z= Serial.read();
-    Serial.print(x);
-    Serial.print(y);
-    Serial.print(z);
-    Serial.write('o');
-    if(y==222 && (x==com_pick || x==com_place)){
-      arm_sequence(z,x%2);
-//      digitalWrite(13,HIGH);
-//      delay(100);
-//      digitalWrite(13,LOW);
-//      delay(100);
+  int _start_byte_ = 0;
+  int command = 0;
+  int parameter = 0;
+  if(Serial.available() == 3) {
+    _start_byte_ = Serial.read();
+    command = Serial.read();
+    parameter = Serial.read();
+  
+    if(_start_byte_ == start_byte) {
+      // _blink_();
+      Serial.write(OKAY_CHARACTER);
+
+      if(command == move_command) {
+        move(parameter);
+        Serial.write(DONE_CHARACTER);
+      }
+      else if(command == pick_command) {
+        pick();
+        Serial.write(DONE_CHARACTER);
+      }
+      else if(command == place_command) {
+        place();
+        Serial.write(DONE_CHARACTER);
+      }
+    }
+
+    else {
+      Serial.write(NOT_OKAY_CHARACTER);
+      //flush buffer
+      while(Serial.available() > 0) {
+        Serial.read();
+      }
     }
   }
+}
+
+void pick() {
+  gripper_servo.write(gripper_servo_release_angle);
+  delay(DELAY);
+  vertical_servo.write(vertical_servo_down_angle);
+  delay(DELAY);
+  gripper_servo.write(gripper_servo_grab_angle);
+  delay(DELAY);
+  vertical_servo.write(vertical_servo_up_angle);
+  delay(DELAY);
+}
+
+void place() {
+  vertical_servo.write(vertical_servo_down_angle);
+  delay(DELAY);
+  gripper_servo.write(gripper_servo_release_angle);
+  delay(DELAY);
+  vertical_servo.write(vertical_servo_up_angle);
+  delay(DELAY); 
+}
+
+void move(int position) {
+  rotation_servo.write(position);
+  delay(10);
+}
+
+void _blink_() {
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
 }
--- working_directory/main_directory/arduino2.py
+++ working_directory/main_directory/arduino2.py
@@ -1,19 +1,87 @@
-from subordinate_directory import serial_ports_setup
-arduino = serial_ports_setup.find_dynamixel_and_arduino()
-# import serial
-# arduino = serial.Serial('/dev/cu.usbmodem1421')
-arduino.baudrate = 57600
+import serial
+import time
+
+#--------------------- ARDUINO SETUP --------------------------
+arduino = serial.Serial('/dev/cu.usbmodem1421')
+# from subordinate_directory import serial_ports_setup
+# arduino = serial_ports_setup.find_dynamixel_and_arduino()
+def decorate_serial_object(serial_object) : 
+	
+	def decorator(function) :
+		def wrapper(*args,**kwargs) : 
+			return_value = None
+			try : 
+				return_value = function(*args,**kwargs)
+			except OSError :
+				print('arduino not connected')
+			else :
+				return return_value
+		return wrapper
+
+	def set_baudrate(baudrate) : 
+		try :
+			serial_object.baudrate = baudrate
+		except serial.serialutil.SerialException :
+			print('arduino not connected')
+	
+	serial_object.write = decorator(serial_object.write)
+	serial_object.read = decorator(serial_object.read)
+	serial_object.inWaiting = decorator(serial_object.inWaiting)
+	serial_object.set_baudrate = set_baudrate
+	return serial_object
+#--------------------------------------------------------------
+
+arduino = decorate_serial_object(arduino)
+arduino.set_baudrate(57600)
+
 GO_TO_SERVO_POS = 0
 
-def write() :
+START_BYTE = 255			#'\xff'
+PICK_COMMAND = 97			# "a"
+PLACE_COMMAND = 98			# "b"
+MOVE_COMMAND = 99			# "c"
+OKAY_CHARACTER = 'O'		# Okay I am doing it
+DONE_CHARACTER = 'D'		# DONE
+NOT_OKAY_CHARACTER = 'N'	# Not Okay
+
+ROTATE_COMMUNICATION_TIMEOUT_LIMIT = 10
+PICK_COMMUNICATION_TIMEOUT_LIMIT = 25
+PLACE_COMMUNICATION_TIMEOUT_LIMIT = 25
+
+def send_and_check(instruction_packet,timeout=5) :
+	print(timeout) 
+	arduino.write(instruction_packet) 
+	start_time = time.time()
+	elapsed_time = 0
+	FLAG = 0
+	while elapsed_time < timeout and FLAG != 2:
+		elapsed_time = time.time() - start_time
+		if arduino.inWaiting() > 0 :
+			returned_data = arduino.read(arduino.inWaiting())
+			if OKAY_CHARACTER in returned_data :
+				FLAG += 1
+			elif DONE_CHARACTER in returned_data :
+				FLAG += 1 
+			elif NOT_OKAY_CHARACTER in returned_data : 
+				arduino.write(instruction_packet)
+	if FLAG != 2 : 
+		raise OSError('arduino not responding')
+		# EXCEPTION HANDLING
+
+def rotate() :
 	global GO_TO_SERVO_POS 
-	instruction_packet = '\x00' + chr(GO_TO_SERVO_POS)
-	arduino.write(instruction_packet)
-	
-def pick() : 
-	arduino.write('\x01')
-	pass
+	instruction_packet = chr(START_BYTE) + chr(MOVE_COMMAND) + chr(GO_TO_SERVO_POS)
+	send_and_check(instruction_packet,timeout = ROTATE_COMMUNICATION_TIMEOUT_LIMIT)
+
+def pick() :
+	instruction_packet = chr(START_BYTE) + chr(PICK_COMMAND) + chr(0)
+	send_and_check(instruction_packet,timeout = PICK_COMMUNICATION_TIMEOUT_LIMIT)
 
 def place() : 
-	arduino.write('\x02')
-	pass
+	instruction_packet = chr(START_BYTE) + chr(PLACE_COMMAND) + chr(0)
+	send_and_check(instruction_packet,timeout = PLACE_COMMUNICATION_TIMEOUT_LIMIT)
+
+def move(pos) : 
+	global GO_TO_SERVO_POS
+	GO_TO_SERVO_POS = pos
+	rotate()
