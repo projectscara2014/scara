206049f3062e482c6d41f0b927a141dd2249511c
ironstein <ironstein1994@gmail.com>
2016-02-16 20:11:00 +0530

    updated __init__.py in main_directory and subordinate_directory to run without needing gui

---
+++ working_directory/main_directory/__init__.py
@@ -0,0 +1,24 @@
+import sys
+
+WORKING_DIRECTORY = ''
+SPLITTING_CHARACTER = ''
+if sys.platform.startswith('win') : 
+	SPLITTING_CHARACTER = '\{}'.format('')
+elif sys.platform.startswith('darwin') : 
+	SPLITTING_CHARACTER = '/'
+
+
+def setup() : 
+
+	def locate_working_directory() : 
+		working_directory = ''
+		for element in __file__.split(SPLITTING_CHARACTER)[:-2] :
+			working_directory += element + '{}'.format(SPLITTING_CHARACTER)
+		return working_directory
+	
+	global WORKING_DIRECTORY
+	WORKING_DIRECTORY = locate_working_directory()
+	print('working_directory --> ',WORKING_DIRECTORY)
+	sys.path.append(WORKING_DIRECTORY)
+
+setup()
