0b53014b1ce67dac535d24d3128cca2a0a515f0b
dhr9 <dhr.reshamwala@gmail.com>
2016-03-02 01:30:13 +0530

    changed flag value and ensured a running system

--- working_directory/comm/comm_init.py
+++ working_directory/comm/comm_init.py
@@ -2,7 +2,7 @@ from utils import serial_ports_setup
 
 arduino1_connected = True
 arduino2_connected = True
-dynamixel_connected = False
+dynamixel_connected = True
 
 if(arduino1_connected and arduino2_connected):
 	[arduino1_serial_object,arduino2_serial_object] = serial_ports_setup.get_connected_arduino_objects(True,True) # CHANGE send True,True
