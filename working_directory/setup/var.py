5a1b3844719289fb9662213ec508fc3b79b69ad7
dhr9 <dhr.reshamwala@gmail.com>
2016-03-03 02:17:44 +0530

    restructured this_to_that and added servo_offset_correction

--- working_directory/setup/var.py
+++ working_directory/setup/var.py
@@ -1,6 +1,7 @@
 #------------------angles_to_coordinates----------------------
 LENGTH_OF_FIRST_ARM  = 24.6 #25.75
 LENGTH_OF_SECOND_ARM = 24.9 #25.3
+GRIPPER_OFFSET_LENGTH = 0.9 #1.0 #1.1
 
 #------------------coordinates_to_angles---------------
 
@@ -12,8 +13,9 @@ SPACING = 3
 BLOCK_DIM = 4
 MAXIMUM_NUMBER_OF_BLOCKS=12
 
-#-----------------maximum_alpha---------------------
-MAXIMUM_ALPHA = 135
+#-----------------maximum_dynamixel_rotation---------------------
+MAXIMUM_ALPHA = 1500 # depends on dynamixel.py
+MAXIMUM_BETA  = 1500 # depends on dynamixel.py
 
 #-----------display_area_blocks_position-------------
 #alpha = 72.7824
